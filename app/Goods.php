7699dafa1a421dabe199a8d66c825bae86268c2a
Your Name <liyonghzeng@2548722906@qq.com>
2019-04-19 10:56:31 +0000

    框架

---
+++ app/Goods.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Goods extends Model
+{
+    protected $table = 'Goods';
+    public $timestamps = false;
+    public $primaryKey = 'id';
+ }
+
