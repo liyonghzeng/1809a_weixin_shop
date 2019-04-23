5360dd5a6f6b7a61575088d906bee2199410e052
Your Name <liyonghzeng@2548722906@qq.com>
2019-04-23 12:18:11 +0000

    debug

--- app/Http/Controllers/JssdkController.php
+++ app/Http/Controllers/JssdkController.php
@@ -14,8 +14,9 @@ class JssdkController extends Controller
         $timestamp = time();
         $nonceStr = Str::random(10);
          $jsapi_ticket = getJsapiTicket();
-        $urll = $_SERVER['REQUEST_SCHEME'].'://' . $_SERVER['HTTP_HOST'] .$_SERVER['REQUEST_URI'];
-        $string1  = "$nonceStr.$jsapi_ticket.$timestamp.$urll";
+        $current_url = $_SERVER['REQUEST_SCHEME'].'://' . $_SERVER['HTTP_HOST'] .$_SERVER['REQUEST_URI'];
+        $string1  =   "jsapi_ticket=$jsapi_ticket&noncestr=$nonceStr&timestamp=$timestamp&url=$current_url";
+
         $sign = sha1($string1);
         $js_config = [
             'appId' => env('WX_APPID'),        //公众号APPID
