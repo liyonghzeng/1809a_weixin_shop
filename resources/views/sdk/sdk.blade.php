5b4e9bd6a2029f9de5e07152f19517ccaf82822a
Your Name <liyonghzeng@2548722906@qq.com>
2019-04-24 00:31:00 +0000

    debug

--- resources/views/sdk/sdk.blade.php
+++ resources/views/sdk/sdk.blade.php
@@ -50,8 +50,8 @@
                             localId: v, // 需要上传的图片的本地ID，由chooseImage接口获得
                             isShowProgressTips: 1, // 默认为1，显示进度提示
                             success: function (res1) {
-                                // var serverId = res1.media_id; // 返回图片的服务器端ID
-                                alert(res1);
+                                var serverId = res1.serverId; // 返回图片的服务器端ID
+                                alert(serverId);
                                 // $.ajax({
                                 //     url : '/test2?img='+serverId,     //将上传的照片id发送给后端
                                 //     type: 'get',
